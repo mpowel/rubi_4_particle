7bc4e9804d604a47d3b433928a069aaccff15e28
mpowel <mlp206@gmail.com>
2017-05-05 14:44:08 -0400

    fix

--- app.rb
+++ app.rb
@@ -75,7 +75,7 @@ get '/incoming_sms' do
     event_data = "foodtype:#{ body }"
     message = "Please enter a number to set the number of days of the timer"
   elsif session["last_context"] == "num_days"
-    session["last_context"] = null
+    session["last_context"] = nil
     event_data = "numdays:#{ body }"
     message = "Your timer is set for #{body} days. Great!"
     
