b5e1c1ae7e40e1d268be79fb0cb1d885c6400dc4
mpowel <mlp206@gmail.com>
2017-05-05 12:45:24 -0400

    .

--- app.rb
+++ app.rb
@@ -1,1 +1,79 @@
-require 'sinatra'
+require 'json'
+require "sinatra"
+require 'shotgun'
+require 'active_support/all'
+require "active_support/core_ext"
+require 'rake'
+# require 'sinatra/activerecord'
+# require 'pg'
+
+require 'twilio-ruby'
+require 'giphy'
+
+# Load environment variables using Dotenv. If a .env file exists, it will
+# set environment variables from that file (useful for dev environments)
+configure :development do
+  require 'dotenv'
+  Dotenv.load
+end
+
+
+# CREATE A CLient
+client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+
+# Use this method to check if your ENV file is set up
+get "/" do
+  "Hello world!"
+end
+
+get "/from" do
+  ENV["TWILIO_FROM"]
+end
+
+# Test sending an SMS
+# change the to to your number 
+get "/send_sms" do
+
+  client.account.messages.create(
+    :from => "+14152002424",
+    :to => "+12067130783",
+    :body => "Hey there. This is a test"
+  )
+
+  "Sent message"
+  
+end
+
+
+# Hook this up to your Webhook for SMS/MMS through the console
+get '/incoming_sms' do
+
+  session["counter"] ||= 0
+  count = session["counter"]
+  
+  sender = params[:From] || ""
+  body = params[:Body] || ""
+  body = body.downcase.strip
+
+  if session["counter"] < 1
+    message = "Thanks for your first message. From #{sender} saying #{body}"
+  else
+    message = "Thanks for message number #{ count }. From #{sender} saying #{body}"
+  end
+  
+  session["counter"] += 1
+  
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message message
+  end
+
+  content_type 'text/xml'
+
+  twiml.text
+
+end
+
+error 401 do 
+  "Not allowed!!!"
+end
+
