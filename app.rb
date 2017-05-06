dbfc28fba81bd8fdea0224587179c649c44cf968
mpowel <mlp206@gmail.com>
2017-05-06 01:27:35 -0400

    too big

--- app.rb
+++ app.rb
@@ -85,19 +85,22 @@ get '/incoming_sms' do
     message = "Notifications have been turned off."
   elsif body.starts_with? "set"
     event_data = "settime:#{ body }"
-    message = "Enter a value 1-30 to set the number of days for the timer."
+    message = "We recommend 4-15 days depending on the food. Type a number to set the timer."
   elsif body_toint < 4
     event_data = "numdays_short:#{ body }"
     message = "Timer set for #{ body } days. You can now place me in the fridge. Fun fact, savethefood.com says aside from raw meats most food can be stored more than 3 days. To reset timer, type any number >3."
   elsif body_toint >= 4
     event_data = "numdays_OK:#{ body }"
     message = "Great! Your timer is set for #{body} days. Go ahead and place me in the fridge."
+  elsif body_toint > 30
+    event_data = "numdays_long:#{ body }"
+    message = "Whoaaaa, #{ body } days?! Lets stay under a month. Enter a number 1-30."
   elsif not defined? session["last_context"] or get_context == nil
       event_data = "error:#{ body}"
       message = "Sorry, I did not get that. Type SET to adjust the timer. Otherwise, put me in the fridge."
   else
     event_data = "numerror:#{ body }"
-    message = "Sorry, I didn't get that. Enter a value 1-30 or place me in the fridge."
+    message = "Sorry, I did not get that. Enter a value 1-30 or place me in the fridge."
   end
 
   particle_client.publish(name: "smart_food/sms/incoming/#{sender}", data: event_data)
