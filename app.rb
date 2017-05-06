a2f4051c05fd555a433aa4887be4ac0fa0da5f91
mpowel <mlp206@gmail.com>
2017-05-06 00:03:40 -0400

    .

--- app.rb
+++ app.rb
@@ -79,7 +79,7 @@ get '/incoming_sms' do
     # session["last_context"] = "set_by_user"
     event_data = "settime:#{ body }"
     message = "Enter a value 1-30 to set the number of days for the timer."
-  elsif body < 4
+  elsif body.include? 1 or body.include? 2 or body.include? 3
     session["last_context"] = nil
     event_data = "numdays_short:#{ body }"
     message = "Timer set for #{ body } days. You can now place me in the fridge. Fun fact, savethefood.com says aside from raw meats most food can be stored more than 3 days. To reset timer, type any number >3."
